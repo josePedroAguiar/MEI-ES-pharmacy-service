530e7fa10b22d712a74007f2e479b17a5690b565
Miguel-Soeiro <101994439+Miguel-Soeiro@users.noreply.github.com>
2023-04-29 12:21:13 +0100

    Routes feitas

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,16 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route, useNavigate, BrowserRouter } from 'react-router-dom';
 import Login from './components/authentication/Login';
 function App() {
 
   return (
-  //   <Routes>
-  //     <Route path="/" element={<Login />} />
-  // </Routes>
-  <Login/>  
-  );
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter >
+      );
 }
 
-export default App;
+      export default App;
