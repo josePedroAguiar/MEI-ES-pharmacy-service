f2359b916159a97c006b708c7ea9b9e04afa7038
José Pedro Aguiar <88104767+josePedroAguiar@users.noreply.github.com>
2023-05-06 17:03:45 +0100

    token

--- server-django/ebdjango/pharmacyapps/views.py
+++ server-django/ebdjango/pharmacyapps/views.py
@@ -1,10 +1,41 @@
 from django.shortcuts import render,redirect ,get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login as l,logout as lo,authenticate
 from django.utils  import   timezone
 from django.contrib.auth.decorators import login_required
+from .models import User
+from datetime import datetime, timedelta
+import jwt
+from django.http import JsonResponse
+from functools import wraps
+from django.core.cache import cache
+
+
+SECRET_KEY= "secretkey"
+
+def token_required(f):
+    @wraps(f)
+    def decorated(request, *args, **kwargs):
+        token = None
+        if 'access-token' in request.headers:
+            token = request.headers['access-token'] 
+                   # Check if token is in the blacklist
+        if not token:
+            return JsonResponse({'message': 'Token is missing!'})
+        if cache.get('access-token' + token):
+            return JsonResponse({'message': 'Token has been revoked!'})
+        try:
+            data = jwt.decode(
+                token, SECRET_KEY, algorithms="HS256")
+            username = data["public_id"]
+            current_user = username
+        except:
+            return JsonResponse({'message': 'Token is invalid!'})
+
+        return f(request, current_user, *args, **kwargs)
+
+    return decorated
 
 
 def home(request):
@@ -32,17 +63,20 @@ def login(request):
     if request.method == 'GET':
         return render(request, 'login.html', {'form': AuthenticationForm()})
     else:
-        user=authenticate(request,username=request.POST['username'],password=request.POST['password'])
+        user = User.objects.filter(username=request.POST['username'],password=request.POST['password']).first()
         if user is None:
             return render(request, 'login.html', {'form': AuthenticationForm(),'error':'Username and password did not match'})
         else:
-            l(request,user)
-            return redirect('currentToDo')
+            user.last_login=datetime.now()
+            user.save()
+            token = jwt.encode({'public_id': user.username, 'exp': datetime.utcnow() + timedelta(minutes=30)}, SECRET_KEY, algorithm="HS256")
+            return JsonResponse({'token': token})
 
-                           
-@login_required 
-def logout(request):
-    if request.method=='POST':
-        lo(request)
-        return redirect('home')
+@token_required                       
+def logout(request,current_user):
+    if request.method=='GET':
+       token = request.META.get('HTTP_ACCESS_TOKEN')
+       if token:
+            cache.set("access-token" + token, True, timeout=None)
+    return JsonResponse({'message': 'Logged out successfully!'})
 # Create your views here.
