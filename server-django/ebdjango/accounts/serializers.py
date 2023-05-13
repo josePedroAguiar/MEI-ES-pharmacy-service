f032aad8c8021f870183b5d2d3af7182d2e36777
Diogo Cleto <sebastiaodiogo1999@gmail.com>
2023-05-13 16:38:07 +0100

    5

--- server-django/ebdjango/accounts/serializers.py
+++ server-django/ebdjango/accounts/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
 #from .models import User
 
 
@@ -10,6 +9,13 @@ class UserSerializer(serializers.ModelSerializer):
     model = User
     fields = ('id', 'username', 'email')
 
+
+def create_user(username, email, password):
+    user = User(username=username, email=email)
+    user.set_password(password)
+    user.save()
+    return user
+
 # Register Serializer
 class RegisterSerializer(serializers.ModelSerializer):
   class Meta:
@@ -18,16 +24,42 @@ class RegisterSerializer(serializers.ModelSerializer):
     extra_kwargs = {'password': {'write_only': True}}
 
   def create(self, validated_data):
-    user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+        username = validated_data['username']
+        email = validated_data['email']
+        password = validated_data['password']
 
-    return user
+        user = create_user(username, email, password)
+
+        return user
+    #user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+
+    #return user
   
+def authenticate_user(username, password):
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return None
+
+    if user.check_password(password):
+        return user
+    else:
+        return None
+
 class LoginSerializer(serializers.Serializer):
   username = serializers.CharField()
   password = serializers.CharField()
 
   def validate(self, data):
-    user = authenticate(**data)
-    if user and user.is_active:
-      return user
-    raise serializers.ValidationError("Incorrect Credentials")
+        username = data.get('username')
+        password = data.get('password')
+
+        user = authenticate_user(username, password)
+
+        if user is None:
+            raise serializers.ValidationError("Incorrect Credentials")
+
+        if not user.is_active:
+            raise serializers.ValidationError("User is inactive.")
+
+        return user
